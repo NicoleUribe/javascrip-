c9cb23855bbe9d9d06a8c3e443342c8579df3f36
Nicole_Uribe <nicole.uribe@ucb.edu.bo>
2022-02-15 21:05:07 -0400

    actualizacion con multiplicar

--- parcel-jest-base-main/src/presenter.js
+++ parcel-jest-base-main/src/presenter.js
@@ -1,4 +1,5 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
@@ -12,4 +13,17 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+const first1 = document.querySelector("#primer_numero");
+const second1 = document.querySelector("#segundo_numero");
+const form1 = document.querySelector("#multiplicar_form");
+const div1 = document.querySelector("#resultado_div");
+
+form1.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber1 = Number.parseInt(first1.value);
+  const secondNumber1 = Number.parseInt(second1.value);
+
+  div1.innerHTML = "<p>" + multiplicar(firstNumber1, secondNumber1) + "</p>";
 });
